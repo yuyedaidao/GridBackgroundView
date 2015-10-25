69a084d2970153343749d7be5f6c71bda0ee8050
yuyedaidao <wyqpadding@gmail.com>
2015-10-25 11:00:23 +0800

    Initial Commit

---
+++ GridBackgroundViewTests/GridBackgroundViewTests.m
@@ -0,0 +1,39 @@
+//
+//  GridBackgroundViewTests.m
+//  GridBackgroundViewTests
+//
+//  Created by 王叶庆 on 15/10/25.
+//  Copyright © 2015年 王叶庆. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GridBackgroundViewTests : XCTestCase
+
+@end
+
+@implementation GridBackgroundViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
